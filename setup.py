f8d41aa8d173cb36dbe6dd579979e29610aa9fac
fcracker79 <fcracker79@gmail.com>
2018-03-12 23:10:01 +0100

    New release 0.0.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_packages(path):
 
 packages = get_packages('yadi')
 setup(name='yadi-framework',
-      version='0.0.4',
+      version='0.0.5',
       description='YADI - Yet another dependency injection framework',
       long_description=readfile('README.rst'),
       url='https://github.com/fcracker79/yadi',
